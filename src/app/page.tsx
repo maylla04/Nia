b58237b2859e78d91f6b0afcc206f408b007b9e1
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-05-15 11:31:10 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Card from './componentes/Card';
+import {Footer} from './componentes/Footer'
+import Header from './componentes/Header';
+import Navbar from './componentes/Navbar';
+
+
+
+
+export default function Home(){
+  return(
+    <div className="h-screen flex flex-col">
+      <Header/>
+      <div className="flex flex-1">
+
+      <Navbar/>
+        <main className="flex-1 p-6">
+
+
+            <h1 className="text-bold text-3xl mt-10">Conheça o meu site!</h1>
+
+  <Card/>
+  
+           
+</main>
+        
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
