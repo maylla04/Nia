c2d91085cc41d969e0457cefece025f677037574
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-05-28 19:23:40 -0300

    Update Card.tsx

--- src/app/componentes/Card.tsx
+++ src/app/componentes/Card.tsx
@@ -13,7 +13,7 @@ export default function Card(){
               <a href="" 
               className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
               <h1 className="font-bold text-2xl mt-2">Objetivos</h1>
-              <h2 className="mt-4 ml-3 mr-2">Aqui fica sobre os objetivos do projeto, um resumo basico</h2>
+              <h2 className="mt-4 ml-3 mr-2">Cumprir a Lei 10639 dentro do IFMS CAMPUS NOVA ANDRADINA</h2>
               </a>
 
 
