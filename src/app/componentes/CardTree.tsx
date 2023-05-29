3cd995f9b416eeb4362f2f863619a7c5bd45b28a
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-05-28 21:21:43 -0300

    Update CardTree.tsx

--- src/app/componentes/CardTree.tsx
+++ src/app/componentes/CardTree.tsx
@@ -1,11 +1,42 @@
 export default function CardTree(){
     return(
         <div>
-            <center><h1 className="mt-5">Religiões Africanas</h1></center>
-        <div className="grid grid-cols-3 gap-4 mt-6">
+           <center><h1 className="mt-5 text-2xl">Musicas Africanas Famosas 2023</h1></center>
+
+<div className="grid grid-cols-3 gap-4 mt-6">
+
+<a href="https://www.youtube.com/watch?v=CQLsdm1ZYAw" 
+className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+<h1 className="font-bold mt-2 text-2xl">Rema - Calm Down</h1>
+<iframe src="https://www.youtube.com/embed/CQLsdm1ZYAw" 
+title="Rema - Calm Down (Official Music Video)" allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="mb-10 mt-10"></iframe>
+</a>
+
+
+<a href="https://www.youtube.com/watch?v=IcIkmPU9reU" 
+className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+<h1 className="font-bold mt-2 text-2xl">Oxlade - KU LO SA</h1>
+<iframe src="https://www.youtube.com/embed/IcIkmPU9reU" 
+title="Oxlade, Camila Cabello - KU LO SA (Official Video)"  allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="mb-10 mt-10"></iframe>
+</a>
+
+
+
+<a href="https://www.youtube.com/watch?v=D-YDEyuDxWU" 
+className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+<h1 className="font-bold mt-2 text-2xl">Ckay - Love Nwantiti</h1>
+<iframe src="https://www.youtube.com/embed/D-YDEyuDxWU" title="CKay - Love Nwantiti Remix ft. Joeboy &amp; Kuami Eugene [Ah Ah Ah] [Official Music Video]" allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="mb-10 mt-10"></iframe>
+</a>
+
+</div>
+            <center><h1 className="mt-10 mb-5 text-2xl">Religiões Africanas</h1></center>
+        <div className="grid grid-cols-2 gap-4 mt-6">
         
               <a href="https://www.educamaisbrasil.com.br/enem/religiao/candomble" 
-              className="bg-white/5 p-1 rounded-md flex flex-col  items-center gap-1 hover:bg-white/10">
+              className="bg-white/5 p-1 rounded-md flex flex-col  items-center gap-2 hover:bg-white/10">
               
               <h1 className="font-bold mt-2 text-2xl">Candoblé</h1>
               <img className="mt-2 rounded" width={220} height={150} 
@@ -27,55 +58,18 @@ export default function CardTree(){
               <a href="" 
               className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
               <h1 className="font-bold mt-2 text-2xl">Ubanda</h1>
-              <h2 className="mt-4 ml-3 mr-2">Aqui fica um resumo basico<br/><br/><br/><br/></h2>
-              </a>
-
-
-
-              <a href="" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold mt-2 text-2xl">a</h1>
-              <h2 className="mt-4 ml-3 mr-2">Aqui fica um resumo basico<br/><br/><br/><br/></h2>
-              </a>
-                </div>
-              <center><h1 className="mt-5">Danças Africanas</h1></center>
-
-              <div className="grid grid-cols-3 gap-4 mt-6">
-
-              <a href="https://mundoeducacao.uol.com.br/educacao-fisica/capoeira.htm" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold mt-2 text-2xl">Capoeira</h1>
               <img className="mt-2 rounded" width={220} height={150} 
-              src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2023/02/homens-lutando-capoeira-em-salvador-na-bahia.jpg"/>
+              src="https://static.todamateria.com.br/upload/um/ba/umbandalagoabb.jpg"/>
+              <h2 className="mt-4 ml-3 mr-2">A Umbanda é uma religião monoteísta e afro-brasileira, surgida em 1908, 
+              fundada por Zélio Fernandino de Moraes. Baseia-se em três 3 conceitos fundamentais: Luz, Caridade e Amor.
+               A palavra "umbanda" pertence ao vocabulário quimbundo, de Angola, e quer dizer "arte de curar".<br/><br/><br/><br/></h2>
+              </a>
+
+
+
+            
+                </div>
               
-              <h2 className="mt-4 ml-3 mr-2">"A capoeira é uma representação cultural afro-brasileira 
-              que mistura esporte, luta, dança, cultura popular, música e brincadeira. 
-              Ela é caracterizada por movimentos ágeis e complexos, nos quais são utilizados os pés, 
-              as mãos e elementos ginástico-acrobáticos. Uma das principais diferenças 
-              da capoeira em relação a outras lutas é o uso da música em sua execução."<br/><br/>
-              CREDITO: MUNDO EDUCAÇÃO
-              <br/><br/>
-
-
-              </h2>
-              </a>
-
-
-              <a href="https://www.youtube.com/watch?v=cr_iazEdgzE&pp=ygUKdHJhcHMgMjAyMw%3D%3D" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold">a</h1>
-              <h2>Aqui fica um resumo basico<br/><br/><br/><br/></h2>
-              </a>
-
-
-
-              <a href="https://www.youtube.com/watch?v=9BSYJGUI_2Q&pp=ygUdYnRzIGRlIGZlcmlhcyBsZWdlbmRhZG8gcHQgYnI%3D" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold">a</h1>
-              <h2>Aqui fica um resumo basico<br/><br/><br/><br/></h2>
-              </a>
-
-            </div>
             </div>
     )
 }
