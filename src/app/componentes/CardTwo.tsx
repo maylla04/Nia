0c629c0f45c9908e1e3640c3f788d7270ffd2631
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-06-05 11:18:04 -0300

    Update CardTwo.tsx

--- src/app/componentes/CardTwo.tsx
+++ src/app/componentes/CardTwo.tsx
@@ -1,40 +1,114 @@
 export default function CardTwo(){
     return(
+        <div>
+            <center><h2 className="font-semibold text-2xl mt-10">Voce sabe o que é racismo?</h2></center>
         <div className="grid grid-cols-3 gap-4 mt-6">
               
-              <a href="https://brasilescola.uol.com.br/o-que-e/o-que-e-sociologia/o-que-e-racismo.htm" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold mt-2">Definição de racismo:</h1>
-              <h2 className="mt-12 ml-3 mr-2">"O racismo é uma forma de preconceito e 
-              discriminação baseada num termo controverso, que sociologicamente 
-              é revisto e do qual a genética também inicia uma revisão: a raça."<br/><br/>
-              CREDITO: BRASIL UOL
+              <a href="https://blogs.uninassau.edu.br/sites/blogs.uninassau.edu.br/files/anexo/racismo_estrutural_feminismos_-_silvio_luiz_de_almeida.pdf" 
+              className="border bg-gradient-to-r from-zinc-950 to-zinc-800 bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+              <h1 className="font-bold mt-2 text-2xl">Definição de racismo:</h1>
+              <h2 className="mt-12 ml-3 mr-2">“É uma forma sistemática de discriminação 
+              que tem a raça como fundamento, e que se manifesta por meio de práticas 
+              conscientes e inconscientes com finalidade em desvantagens ou primilégios 
+              para individuos, a depender do grupo racial ao qual pertençam ” 
               
+              <br/><br/>
+              <b>Escrito por Almeida, 2019</b>
+              </h2>
+              
+              </a>
+
+              <a href="https://www.todamateria.com.br/racismo/" 
+              className="border bg-gradient-to-r from-zinc-800 to-zinc-900 bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+              <h1 className="font-bold mt-2 text-2xl">Tipos de racismo:</h1>
+              <h2 className="mt-12 ml-3 mr-2">1. Racismo Individual<br/><br/>
+              2. Racismo Institucional<br/><br/>
+              3. Racismo Cultural<br/><br/>
+              4. Racismo Comunitarista (Diferencialista)<br/><br/>
+              5. Racismo Ecológico (Ambiental)<br/><br/>
+              
+              <br/><br/>
+              <b>Escrito por Juliana Bezerra</b>
+              
+
+
+              </h2><br/>
+              
+              </a>
+
+              <a href="https://blog.archtrends.com/o-que-e-racismo-estrutural/" 
+              className="border bg-gradient-to-r from-zinc-800 to-zinc-950 bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+              <h1 className="font-bold mt-2 text-2xl">Racismo Estrutural</h1>
+              <h2 className="mt-12 ml-3 mr-2">“Racismo estrutural é o termo utilizado para mostrar que o racismo 
+              está enraizado em nossa sociedade, ou seja, no direito, na economia, na ideologia, na política. 
+              Ele se mostra a partir de práticas, hábitos e falas cotidianas. Isso significa que, se o país onde 
+              vivemos foi construído com base em ideais racistas, o racismo deixa de se expressar como uma 
+              anormalidade e se torna um componente significativo pra sociedade.” 
+              
+              <br/><br/>
+              <b>Escrito por Mariany Alves Bittencourt</b>
+              
+
+
+              </h2><br/>
+              
+              </a>
+
+                
+            </div>
+            
+        <div className="grid grid-cols-3 gap-4 mt-10">
+              
+              <a href="https://brasilescola.uol.com.br/o-que-e/o-que-e-sociologia/o-que-e-racismo.htm" 
+              className="border bg-gradient-to-r from-zinc-950 to-zinc-800 bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
+              <h1 className="font-bold mt-2 text-2xl">Dia da Consciência Negra</h1>
+              <h2 className="mt-12 ml-3 mr-2">
+              “O Dia da Consciência Negra busca valorizar conquistas e o protagonismo negro na história política do Brasil
+              , além de ser um marco importante na luta contra o racismo estrutural. A origem dessa data, 
+              que também se propõe reflexiva, nos remete aos anos 70. O Movimento Negro Unificado teve papel 
+              fundamental nesse percurso, difundindo a cultura negra e sendo um importante agente contra o racismo, 
+              inspirado na luta anti-apartheid na África do Sul.” 
+              <br/><br/>
+              
+              <b>Escrito por Gabriella Garcia</b>
 
 
               </h2>
               </a>
 
 
-              <a href="https://www.campograndenews.com.br/brasil/cidades/crimes-de-racismo-cresceram-128-em-ms" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold mt-2">Casos de racismo no:</h1>
-              <h1 className="font-bold">MS</h1>
-              <h2 className="mt-4 ml-3 mr-2">"Dados da Sejusp (Secretaria de Estado de Justiça e 
-              Segurança Pública) mostraram que os crimes de racismo em Mato Grosso do Sul 
-              cresceram 128% no ano de 2022, em comparação a 2021. Enquanto em 2022 foram 
-              notificados 80 ocorrências, em 2021 foram 35."<br/><br/> CREDITO: CAMPO GRANDE NEWS</h2>
+              <a href="https://www.geledes.org.br/6-maneiras-de-combater-a-discriminacao-racial-ainda-na-infancia/" 
+              className="border bg-gradient-to-r from-zinc-800 to-zinc-900 bg-white/5 p-1 rounded-md flex 
+              flex-col items-center gap-1 hover:bg-white/10">
+              <h1 className="font-bold flex text-center text-2xl">6 maneiras de combater a discriminação racial ainda na:</h1>
+              <h1 className="font-bold text-2xl">Infância</h1>
+              <h2 className="mt-4 ml-3 mr-2">1. Atente-se para não reproduzir comportamentos preconceituosos;<br/><br/>
+              2. Repense seus atos e seu vocabulário;<br/><br/>
+              3. Não incentive apelidos que caracterizam estereótipos;<br/><br/>
+              4. Reflita a necessidade de festas em datas comemorativa;<br/><br/>
+              5. Explique, explique e explique tudo às crianças;<br/><br/>
+
+              6. Valorize a diversidade no dia a dia.<br/><br/> <b>Por FLÁVIA BEZERRA, do Ceert.</b></h2><br/>
               </a>
 
 
 
               <a href="" 
-              className="bg-white/5 p-1 rounded-md flex flex-col items-center gap-1 hover:bg-white/10">
-              <h1 className="font-bold mt-2">Casos de racismo no:</h1>
-              <h1 className="font-bold">IFMS</h1>
-              <h2 className="mt-4 ml-3 mr-2">Aqui fica um resumo basico<br/><br/><br/><br/></h2>
+              className="border bg-gradient-to-r from-zinc-800 to-zinc-950 bg-white/5 p-1 rounded-md 
+              flex flex-col items-center gap-1 hover:bg-white/15">
+              <h1 className="font-bold mt-2 text-2xl">Racismo é:</h1>
+              <h1 className="font-bold text-2xl">CRIME!</h1>
+              <h2 className="mt-4 ml-3 mr-2">“A Lei 7.716/89, conhecida com Lei do Racismo, 
+              pune todo tipo de discriminação ou preconceito, seja de origem, raça, sexo, cor, idade. 
+              Em seu artigo 3º, a lei prevê como conduta ilícita o ato de impedir ou dificultar que alguém tenha 
+              acesso a cargo público ou seja promovido, tendo como motivação o preconceito ou discriminação. 
+              Por exemplo, não deixar que uma pessoa assuma determinado cargo por conta de raça ou gênero. 
+              A pena prevista é de 2 a 5 anos de reclusão.” <br/><br/><br/><br/>
+              <b>Tribunal de Justiça do Distrito Federal e dos Territórios, 2021</b></h2>
               </a>
 
+                
+            </div>
             </div>
     )
 }
