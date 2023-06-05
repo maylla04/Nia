714883789f6144e8b9c2dfb892fdac0e6623f381
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-06-05 11:34:04 -0300

    Update page.tsx

--- src/app/Denuncie/page.tsx
+++ src/app/Denuncie/page.tsx
@@ -11,30 +11,36 @@ export default function Denuncie(){
 
       <Navbar/>
         <main className="flex-1 p-6">
-        <form className="ml-10" method="post" encType="multipart/form-data">
-            <h1 className="font-semibold text-2xl mt-10 mb-5">Formulário de Denúncia:</h1>
+        <form className="" method="post" encType="multipart/form-data">
+            <h1 className="ml-10 font-semibold text-2xl mt-5 mb-5">Formulário de Denúncia:</h1>
             
-            <input className="text-black mt-5 mb-10 w-80 border-b bg-black" type="text" placeholder="Digite seu nome completo" required/><br/>
+            <input className="ml-10 text-black mt-5 mb-5 w-80 border-b bg-zinc-800" 
+            type="text" placeholder="Digite seu nome completo"/><br/>
 
             
-            <input className="text-black mt-5 mb-10 w-80 border-b bg-black" type="text" placeholder="Digite seu e-mail" required/><br/>
+            <input className="ml-10 text-black mt-5 mb-5 w-80 border-b bg-zinc-800" 
+            type="text" placeholder="Digite seu e-mail"/><br/>
 
-            <label>Selecione o docente para envio<br/> 
-              (opcional):</label><br/>
-            <select className="mt-3 mb-5 border-b bg-black w-80">
+            <label className="ml-10">Quem voce quer que leia sua denúncia?
+              </label><br/>
+            <select className="ml-10 mt-3 mb-2 border-b bg-zinc-800 w-80">
+            <option>NEABI</option>
+            <option>Nenhum</option>
               <option>Marcela Ernesto</option>
               <option>Jales Lucio</option>
             </select><br/>
 
-            <input className="mt-5 mb-10 w-80 h-20 border-b bg-black" 
+            <input className="ml-10 mt-5 mb-5 w-80 h-20 border-b bg-zinc-800" 
             type="text" placeholder="Digite aqui sua denúncia..." required/><br/>
 
-            <label>Anexar arquivos:</label><br/>
+            <label className="ml-10">Anexar arquivos:</label><br/>
 
-            <input type="file" className="mt-5"/><br/>
+            <input type="file" className="mt-5 ml-10 "/><br/>
             
-            <input type="submit" placeholder="Enviar" className="bg-zinc-900 w-80 h-14 mt-10"/>
+            <input type="submit" placeholder="Enviar" className="bg-zinc-900 w-80 h-14 mt-10 ml-10 "/>
             </form> 
+            <img className="mt-10"  src="/denuncie.png" alt="Logo"/>
+           
            
            
 </main>
